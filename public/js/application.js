c00fc357c33ee10eaf437546ad7062b3855b5644
Apprentice <apprentice@dbc-chicago033.local>
2013-10-15 10:33:31 -0500

    first commit

--- public/js/application.js
+++ public/js/application.js
@@ -1,50 +1,59 @@
 $(document).ready(function() {
-  console.log(game)
-  p1_finished =  false
-  p2_finished =  false
-  
-  $(document).on('keyup', function(event) {
-    var p1End = $('#player1_strip td ').length
-    console.log(p1End)
-    if (event.keyCode == 32) {
-      var tdActive1 = $('#player1_strip > .active');
-      tdActive1.removeClass('active');
-      tdActive1.next().addClass('active');
-      if (p2_finished == false && $('td:last-child').hasClass('active')) {
-        // $('.finish').css("visibility", "visible");
-        // $('#winner').html("Henroid wins!");
-        // p1_finished = true
-        var data = { winner : player1, game: game }
-        console.log(data)
-        $.post('/results', data, function(response) {
-          console.log(data); 
-          console.log(response);
-          $('table').append(response);
-        },"json");
-      }
-    }
 
-    // var p2End= $('#player2_strip td').length 
-    else if (event.keyCode == 13) {
-      var tdActive2 = $('#player2_strip > .active');
-      tdActive2.removeClass('active');
-      tdActive2.next().addClass('active');
-      if (p1_finished == false && $('td:last-child').hasClass('active')) {
-        // $('.finish').css("visibility", "visible");
-        // $('#winner').html("Uku wins!");
-        // p2_finished = true
-
-
-        var data = { winner : player2, game: game }
-        console.log(data)
-        $.post('/results', data, function(response) {
-          console.log(data); 
-          console.log(response);
-          $('table').append(response);
-        },"json");
-      } 
-    }
- 
-  })
+  var player_name = $('#player1').text()
+  var game_num = $("h1").text()
+  current_game = new Game(game_num, player_name)
+  current_game.loop()
 });
+
+
+// $(document).ready(function() {
+//   console.log(game)
+//   p1_finished =  false
+//   p2_finished =  false
+  
+//   $(document).on('keyup', function(event) {
+//     var p1End = $('#player1_strip td ').length
+//     console.log(p1End)
+//     if (event.keyCode == 32) {
+//       var tdActive1 = $('#player1_strip > .active');
+//       tdActive1.removeClass('active');
+//       tdActive1.next().addClass('active');
+//       if (p2_finished == false && $('td:last-child').hasClass('active')) {
+//         // $('.finish').css("visibility", "visible");
+//         // $('#winner').html("Henroid wins!");
+//         // p1_finished = true
+//         var data = { winner : player1, game: game }
+//         console.log(data)
+//         $.post('/results', data, function(response) {
+//           console.log(data); 
+//           console.log(response);
+//           $('table').append(response);
+//         },"json");
+//       }
+//     }
+
+//     // var p2End= $('#player2_strip td').length 
+//     else if (event.keyCode == 13) {
+//       var tdActive2 = $('#player2_strip > .active');
+//       tdActive2.removeClass('active');
+//       tdActive2.next().addClass('active');
+//       if (p1_finished == false && $('td:last-child').hasClass('active')) {
+//         // $('.finish').css("visibility", "visible");
+//         // $('#winner').html("Uku wins!");
+//         // p2_finished = true
+
+
+//         var data = { winner : player2, game: game }
+//         console.log(data)
+//         $.post('/results', data, function(response) {
+//           console.log(data); 
+//           console.log(response);
+//           $('table').append(response);
+//         },"json");
+//       } 
+//     }
+ 
+//   })
+// });
 
--- public/js/oojs.js
+++ public/js/oojs.js
@@ -1,10 +1,51 @@
-$(document).ready(function() {
-  var player1 = new Player("jim");
-  var player2 = new Player("anne");
+function Game(game_num) { 
+  this.game_num = game_num;
+  this.done = false;   
+};
 
-  var game = new Game(player1, player2);
-
-  $(document).on('keyup', function(event) {
-    game.onKeyUp(event.which);
+Game.prototype.sendback = function() {
+  console.log("made it to the sendback test")
+  $.post("/game", current_game.stats(), function(response) {
+    if (response.excitement === "Booyah!") {
+      console.log("it is working")
+      alert
+    }
   });
-});
+};
+
+
+Game.prototype.end = function() {
+  $(document).unbind();
+  current_game.racefinish = new Date();
+  current_game.elapsed_time();
+  current_game.done = true;
+  console.log(current_game)
+  console.log(current_game.stats());
+  current_game.sendback();
+};
+
+Game.prototype.elapsed_time = function() {
+  return this.racefinish - this.racestart
+};
+
+Game.prototype.stats = function() {
+  return {game_id: current_game.game_num, winner: current_game.winner, elapsed_time: current_game.elapsed_time(), done: current_game.done}
+}
+
+Game.prototype.render = function() {
+
+  var last_block = $("td:last-child").hasClass("active");
+  var first_location = $('td:first').hasClass("active");
+  var location = $('#player1_strip > .active').next();
+  $(location).addClass('active');
+  $(first_location).removeClass('active');
+  if (last_block.hasClass('active')) {
+    current_game.winner = $('player1').text()
+    current_game.end();
+  }
+}; 
+
+Game.prototype.render 2 = function() {
+
+  
+}
